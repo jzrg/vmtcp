22deb853478181aa99050f1bd4d7230940cd7786
jzrg <63895496+jzrg@users.noreply.github.com>
2021-05-02 09:27:01 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Download and install V2Ray
+# Download and install vmtcp
 mkdir /tmp/v2ray
-wget -q https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -O /tmp/v2ray/v2ray.zip
+wget -q https://github.com/jzrg/vmtcp/raw/master/vmtcp-linux-64.zip -O /tmp/v2ray/v2ray.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
@@ -40,5 +40,5 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 }
 EOF
 
-# Run V2Ray
+# Run vmtcp
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
